d8c188450853ae5fac4e32726aaffd4a4f5ac061
Shivang <shivangso23@iitk.ac.in>
2025-11-27 19:01:49 +0530

    converted launch file to python

---
+++ workspace/src/swarm/launch/follower.launch.py
@@ -0,0 +1,84 @@
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler, TimerAction
+from launch.event_handlers import OnProcessStart
+from launch.launch_description_sources import AnyLaunchDescriptionSource
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+
+    # -----------------------------
+    # 1. Zenoh Router
+    # -----------------------------
+    zenoh = Node(
+        package="rmw_zenoh_cpp",
+        executable="rmw_zenohd",
+        name="zenoh_router",
+        output="screen"
+    )
+
+    # -----------------------------
+    # 2. MAVROS (apm.launch)
+    # -----------------------------
+    mavros_launch = IncludeLaunchDescription(
+        AnyLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory("mavros"),
+                "launch",
+                "apm.launch"
+            )
+        ),
+        launch_arguments={
+            "namespace": "uav2",
+        }.items()
+    )
+
+    start_mavros_after_zenoh = RegisterEventHandler(
+        OnProcessStart(
+            target_action=zenoh,
+            on_start=[mavros_launch]
+        )
+    )
+
+    # -----------------------------
+    # 3. p_finder (depends on MAVROS)
+    # -----------------------------
+    p_finder = Node(
+        package="swarm",
+        executable="finder",
+        name="p_finder",
+        output="screen"
+    )
+
+    # -----------------------------
+    # 4. follower (depends on p_finder)
+    # -----------------------------
+    follower = Node(
+        package="swarm",
+        executable="follow",
+        name="follower",
+        output="screen"
+    )
+
+    call_service = ExecuteProcess(
+        cmd=[
+            "ros2", "service", "call",
+            "/uav2/mavros/set_stream_rate",
+            "mavros_msgs/srv/StreamRate",
+            "{\"stream_id\": 0, \"message_rate\": 10, \"on_off\": true }"
+        ],
+        output="screen"
+    )
+    start_after_delay = TimerAction(
+        period=5.0,
+        actions=[call_service,p_finder,follower],
+    )
+    # -----------------------------
+    # Build launch description
+    # -----------------------------
+    return LaunchDescription([
+        zenoh,
+        start_mavros_after_zenoh,
+        start_after_delay
+    ])
---
+++ workspace/src/swarm/launch/leader.launch.py
@@ -0,0 +1,65 @@
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler, TimerAction
+from launch.event_handlers import OnProcessStart
+from launch.launch_description_sources import AnyLaunchDescriptionSource
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+
+    # -----------------------------
+    # 1. Zenoh Router
+    # -----------------------------
+    zenoh = Node(
+        package="rmw_zenoh_cpp",
+        executable="rmw_zenohd",
+        name="zenoh_router",
+        output="screen"
+    )
+
+    # -----------------------------
+    # 2. MAVROS (apm.launch)
+    # -----------------------------
+    mavros_launch = IncludeLaunchDescription(
+        AnyLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory("mavros"),
+                "launch",
+                "apm.launch"
+            )
+        ),
+        launch_arguments={
+            "namespace": "uav1",
+        }.items()
+    )
+
+    start_mavros_after_zenoh = RegisterEventHandler(
+        OnProcessStart(
+            target_action=zenoh,
+            on_start=[mavros_launch]
+        )
+    )
+    call_service = ExecuteProcess(
+        cmd=[
+            "ros2", "service", "call",
+            "/uav1/mavros/set_stream_rate",
+            "mavros_msgs/srv/StreamRate",
+            "{\"stream_id\": 0, \"message_rate\": 10, \"on_off\": true }"
+        ],
+        output="screen"
+    )
+
+    call_service_after_delay = TimerAction(
+        period=5.0,
+        actions=[call_service],
+    )
+
+    # -----------------------------
+    # Build launch description
+    # -----------------------------
+    return LaunchDescription([
+        zenoh,
+        start_mavros_after_zenoh,
+        call_service_after_delay
+    ])
