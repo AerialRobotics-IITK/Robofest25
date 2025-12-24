2a4ac94957c98d6e57bc0317921288df7d16e644
Shivang <shivangso23@iitk.ac.in>
2025-12-24 06:19:34 +0530

    added mavros launch script

---
+++ workspace/src/swarm/launch/mavros.launch.py
@@ -0,0 +1,66 @@
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler, TimerAction
+from launch.event_handlers import OnProcessStart
+from launch.launch_description_sources import AnyLaunchDescriptionSource
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+
+    # -----------------------------
+    # 1. Zenoh Router
+    # -----------------------------
+    zenoh = Node(
+        package="rmw_zenoh_cpp",
+        executable="rmw_zenohd",
+        name="zenoh_router",
+        output="screen"
+    )
+
+    # -----------------------------
+    # 2. MAVROS (apm.launch)
+    # -----------------------------
+    mavros_launch = IncludeLaunchDescription(
+        AnyLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory("mavros"),
+                "launch",
+                "apm.launch"
+            )
+        ),
+        launch_arguments={
+            "namespace": f"uav{os.environ.get('MAV_ID',1)}",
+            "tgt_system": str(os.environ.get("MAV_ID",1)),
+            "fcu_url": os.environ.get('FCU_URL','/dev/ttyACM0'),
+        }.items()
+    )
+
+    start_mavros_after_zenoh = RegisterEventHandler(
+        OnProcessStart(
+            target_action=zenoh,
+            on_start=[mavros_launch]
+        )
+    )
+    call_service = ExecuteProcess(
+        cmd=[
+            "ros2", "service", "call",
+            f"/uav{os.environ.get('MAV_ID',1)}/set_stream_rate",
+            "mavros_msgs/srv/StreamRate",
+            "{\"stream_id\": 0, \"message_rate\": 10, \"on_off\": true }"
+        ],
+        output="screen"
+    )
+    call_service_after_delay = TimerAction(
+        period=5.0,
+        actions=[call_service],
+    )
+
+    # -----------------------------
+    # Build launch description
+    # -----------------------------
+    return LaunchDescription([
+        zenoh,
+        start_mavros_after_zenoh,
+        call_service_after_delay
+    ])
